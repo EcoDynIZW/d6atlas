051c699842723cc64b76c6908b3465032699e41b
Moritz Wenzler-Meya <38684163+wenzmo@users.noreply.github.com>
2023-03-02 14:46:57 +0100

    added dist_water to package and stack

--- R/shapefiles.R
+++ R/shapefiles.R
@@ -46,6 +46,10 @@ dist_r_ras <-
   terra::rast("./inst/extdata/distance-to-rivers_atlas_2009_1m_03035.tif") %>% terra::aggregate(fact = 5, fun = "mean")
 dist_s_ras <-
   terra::rast("./inst/extdata/distance-to-streets_atlas_2022_1m_03035.tif") %>% terra::aggregate(fact = 5, fun = "mean")
+dist_w_ras <-
+  terra::rast("./inst/extdata/distance-to-waterbodies_atlas_2009_1m_03035.tif") %>% terra::aggregate(fact = 5, fun = "mean")
+
+
 
 ras_list <- list(crs = "+proj=laea +lat_0=52 +lon_0=10 +x_0=4321000 +y_0=3210000 +ellps=GRS80 +units=m +no_defs",
                  extent = c(4565438, 4579781, 3362903, 3376728))
@@ -53,13 +57,15 @@ ras_list <- list(crs = "+proj=laea +lat_0=52 +lon_0=10 +x_0=4321000 +y_0=3210000
 landuse <-
   terra::values(lu_ras)[, 1]
 dist_human_settlements <-
-  terra::values(dist_hs_ras)[, 1]
+  terra::values(dist_hs_ras)[, 1] %>% round(digits = 0)
 dist_kettleholes <-
   terra::values(dist_kh_ras)[, 1] %>% round(digits = 0)
 dist_rivers <-
   terra::values(dist_r_ras)[, 1] %>% round(digits = 0)
 dist_streets <-
   terra::values(dist_s_ras)[, 1] %>% round(digits = 0)
+dist_water <-
+  terra::values(dist_w_ras)[, 1] %>% round(digits = 0)
 
 #saveRDS(ras_list, "./data-raw/ras_list.rds")
 #saveRDS(landuse, "./data-raw/landuse.rds")
@@ -68,22 +74,25 @@ dist_streets <-
 #saveRDS(dist_rivers, "./data-raw/dist_rivers.rds")
 #saveRDS(dist_streets, "./data-raw/dist_streets.rds")
 
-usethis::use_data(water_01,
-                  water_02,
-                  ruderal_03,
-                  swamp_04,
-                  grassland_05,
-                  bushland_07,
-                  forest_08,
-                  agriculture_09,
-                  garden_graveyard_10,
-                  built_up_12,
-                  ras_list,
-                  landuse,
-                  dist_human_settlements,
-                  dist_kettleholes,
-                  dist_rivers,
-                  dist_streets,
-                  starling_2547,
-                  overwrite = TRUE)
+# usethis::use_data(
+#   water_01,
+#   water_02,
+#   ruderal_03,
+#   swamp_04,
+#   grassland_05,
+#   bushland_07,
+#   forest_08,
+#   agriculture_09,
+#   garden_graveyard_10,
+#   built_up_12,
+#   ras_list,
+#   landuse,
+#   dist_human_settlements,
+#   dist_kettleholes,
+#   dist_rivers,
+#   dist_streets,
+#   dist_water,
+#   starling_2547,
+#   overwrite = TRUE
+# )
 
