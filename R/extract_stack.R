0ed5584ce31b01827ccd5828d14e6052364698dd
Moritz Wenzler-Meya <38684163+wenzmo@users.noreply.github.com>
2023-03-09 10:01:13 +0100

    added dplyr namesspace

---
+++ R/extract_stack.R
@@ -0,0 +1,50 @@
+#' @title get a dataframe with the extracted values from the atlas_stack
+#' @description add extracted values from atlas_stack (landuse, distance to settlements, distance to kettleholes, distance to rivers, distance to streets, distance to waterbodies) to the starling_2547 dataframe or to another one
+#' @param sf_point a sf point with extent of atlas_stack.
+#' @return a af object with extracted values
+#' @export
+#' @examples
+#' \dontrun{
+#' d6atlas::extract_stack(sf_point = sf_point)
+#'
+#' extract_stack(sf::st_as_sf(
+#'   d6atlas::starling_2547,
+#'   coords = c("X", "Y"),
+#'   crs = sf::st_crs(32633)
+#' ))
+#'  }
+
+
+extract_stack <- function(sf_point){
+
+  stopifnot("sf_point must be an sf point object"= all(sf::st_geometry_type(sf_point) %in% c("POINT", "MULTIPOINT")))
+
+  atlas_stack <- d6atlas::atlas_stack()
+  crs_sf_point <- sf::st_crs(sf_point)
+
+  message("crs was changed to EPSG 3035 for extracting")
+
+  sf_point <- sf_point %>%
+    sf::st_transform(sf::st_crs(atlas_stack))
+
+  sf_point <- dplyr::bind_cols(sf_point, terra::extract(atlas_stack, sf_point, ID = FALSE))
+
+  sf_point <- sf_point %>%
+    sf::st_transform(crs_sf_point)
+
+  sf_point <- sf_point %>%
+    dplyr::left_join(y = d6atlas::landuse_reclass_table %>%
+                       dplyr::rename("landuse" = BIOTYP8) %>%
+                       dplyr::mutate(landuse = as.numeric(landuse)), by = "landuse")
+
+
+  message("crs was changed back to given projection")
+
+  return(sf_point)
+
+}
+
+
+
+
+
